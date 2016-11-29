e309e81868ca23517ba7c0ccb4c752f349c050aa
Quorusa <cyrielle.schaef@gmail.com>
2016-11-29 14:03:57 -0500

    initial commit

---
+++ app/src/main/java/mobile/informatique/cgmatane/qc/ca/miseenforme/MiseEnForme.java
@@ -0,0 +1,13 @@
+package mobile.informatique.cgmatane.qc.ca.miseenforme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MiseEnForme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_mise_en_forme);
+    }
+}
