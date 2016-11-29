59c8762ce74c4fe6cb24f81fcdc5dbfd44bb6e11
Quorusa <cyrielle.schaef@gmail.com>
2016-11-29 16:12:14 -0500

    v1 du modèle

---
+++ app/src/main/assets/www/modele/Joueur.js
@@ -0,0 +1,11 @@
+var Joueur = function(id, nom, nbForme)
+{
+    this.construire = function()
+    {
+        this.id = id;
+        this.nom = nom;
+        this.nbForme = Forme;
+    }
+
+    this.construire();
+}
